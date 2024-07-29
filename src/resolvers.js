b0ace14b65724a89e793302b415be89693c59773
tal_l <tal9lev@gmail.com>
2024-07-29 05:09:18 +0300

    feat: :sparkles: adding option to get data of a repostory

--- src/resolvers.js
+++ src/resolvers.js
@@ -14,8 +14,8 @@ import pLimit from "p-limit";
 export const resolvers = {
   Query: {
     repositories: async () => await getRepositoriesDetails(),
-    repositorie: async (_, { repositoryName }) =>
-      repos.find((repo) => repo.repositoryName === repositoryName),
+    repositorie: async (_, { name }) =>
+      fetchRepositoryAdvanceDetails(name),
   },
 };
 
@@ -24,20 +24,20 @@ async function getRepositoriesDetails() {
   const repositories = JSON.parse(process.env.REPOSITORIES);
   let promises = repositories.map((repo) => {
     return limit(() =>
-      fetchGithubRepositoriessData(repo.owner, repo.repo, false),
+      fetchGithubRepositoriessData(repo.owner, repo.name, false),
     );
   });
 
   const result = await Promise.all(promises);
   return result;
 }
-async function fetchGithubRepositoriessData(owner, repo) {
+async function fetchGithubRepositoriessData(owner, name) {
   let repoData = {};
   try {
-    repoData = await getRepoDetails(owner, repo);
+    repoData = await getRepoDetails(owner, name);
   } catch (error) {
     console.error(`Error fetching repository from github: ${error.message}`);
-    return { name: `error fetching repo:${repo}`, owner: owner, size: -1 };
+    return { name: `error fetching repository name:${name}`, owner: owner, size: -1 };
   }
   return {
     name: repoData.name,
@@ -45,19 +45,22 @@ async function fetchGithubRepositoriessData(owner, repo) {
     owner: repoData.owner.login,
   };
 }
-async function fetchRepoAdvanceDetails(owner, repo) {
-  const repoData = await getRepoDetails(owner, repo);
-  const contents = await getRepoContents(owner, repo);
-
-  //todo fix  yml dont work for repo a and c
-  const ymlFiles = await searchYamlFiles(owner, repo);
-
+async function fetchRepositoryAdvanceDetails(repostoryName) {
+  const repositories=JSON.parse(process.env.REPOSITORIES)
+  const currentRepo = repositories.find((repostory) => repostory.name === repostoryName);
+  if (!currentRepo) {
+    throw new Error(`Repository ${repostoryName} not found`);
+  }
+  const {owner, name} = currentRepo
+  const repoData = await getRepoDetails(owner, name);
+  const contents = await getRepoContents(owner, name);
+  const ymlFiles = await searchYamlFiles(owner, name);
   let ymlContent = "";
 
   if (ymlFiles.length > 0) {
-    ymlContent = await getYmlFileContent(owner, repo, ymlFiles[0]);
+    ymlContent = await getYmlFileContent(owner, name, ymlFiles[0]);
   }
 
-  const webhooks = await getRepoWebhooks(owner, repo);
-  return { repoData, contents, ymlContent, webhooks };
+  const webhooks = await getRepoWebhooks(owner, name);
+  return {name: repoData.name, size: repoData.size, owner: repoData.owner.login, publicStatus: !repoData.private, numberOfFiles: contents.length, yamlContent: ymlContent, webhooks: webhooks};
 }
